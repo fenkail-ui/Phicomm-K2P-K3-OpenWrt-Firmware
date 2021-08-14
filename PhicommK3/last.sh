cd6ccac1ee7f0f0d5056812a1923557cf0380c5f
fenkail-ui <64298897+fenkail-ui@users.noreply.github.com>
2021-08-14 18:34:24 +0800

    Update last.sh

--- PhicommK3/last.sh
+++ PhicommK3/last.sh
@@ -17,7 +17,7 @@
 #
 
 # Alter default router IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 #修改NTP设置
 #sed -i "s/'0.openwrt.pool.ntp.org'/'ntp1.aliyun.com'/g" package/base-files/files/bin/config_generate
